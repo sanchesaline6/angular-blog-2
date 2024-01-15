4ff96a1ca0eb8c737cfedaaac9f9ec499c0105e4
Aline <sanches.aline6@gmail.com>
2024-01-15 01:39:28 -0300

    adiciona componente box-promo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BoxPromoComponent } from './box-promo/box-promo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BoxPromoComponent
   ],
   imports: [
     BrowserModule,
